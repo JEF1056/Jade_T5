d310770c7ea2572c46c909b41d80920c68ab69cb
Jess Edmund Fan <jessefan2002@gmail.com>
2021-01-29 18:29:57 -0800

    Update train.py

--- train.py
+++ train.py
@@ -36,9 +36,11 @@ import src.createtask
 args.tpu_address = f"grpc://{args.tpu_address}:8470"
 
 if args.tpu_address != None:
-    resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu=args.tpu_address)
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver(tpu=args.tpu_address)
     tf.enable_eager_execution()
-    tf.config.experimental_connect_to_cluster(resolver)
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    tpu_strategy = tf.distribute.TPUStrategy(tpu)
     print('Running on TPU:', args.tpu_address)
 
 MODEL_SIZE = args.model_size
