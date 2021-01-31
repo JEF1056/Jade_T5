163203c231f8cf56c43a7e94528a2190ce806cb5
Jess Edmund Fan <jessefan2002@gmail.com>
2021-01-31 12:00:58 -0800

    Update createtask.py

--- src/createtask.py
+++ src/createtask.py
@@ -23,6 +23,8 @@ for ex in tfds.as_numpy(nq_dataset_fn("validation").take(5)):
 
 def preprocess(ds):
     def sample(text):
+        print(text)
+        exit()
         text=tf.strings.unicode_encode(text, "UTF-8")
         text=tf.strings.split(text, sep="\t")
         ind=np.sort(np.random.choice(len(text)-1,2, replace=False))
