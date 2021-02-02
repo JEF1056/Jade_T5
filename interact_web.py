abb948ba8ba6ba97fbcb6577d34f714cf8c8271b
Jess Edmund Fan <jessefan2002@gmail.com>
2021-02-01 16:57:19 -0800

    Update interact_web.py

--- interact_web.py
+++ interact_web.py
@@ -1,8 +1,13 @@
 import json
 import requests
 import argparse
-from src.helpers import str2bool as s2b
 
+def s2b(v):
+    if isinstance(v, bool): return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'): return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'): return False
+    else: raise argparse.ArgumentTypeError('Boolean value expected.')
+    
 parser = argparse.ArgumentParser(description='Export checkpoints for serving')
 parser.add_argument('-url', type=str, default="https://woz-model.herokuapp.com/v1/models/jade:predict",
                     help='Link to the server hosting the model')
@@ -16,7 +21,7 @@ username=input('Username: ')
 while True:
     inp = input('> ')
     inp= username+": "+inp
-    inpdata = '{"inputs": ["Input: '+'\b'.join(history+[inp])+']}'
+    inpdata = '{"inputs": ["Input: '+'\\b'.join(history+[inp])+'"]}'
     response = requests.post(args.url.encode("utf-8"), data=inpdata.encode("utf-8"))
     message = json.loads(response.text)
     if "error" in message or args.debug: 
