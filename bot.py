74c7e3919118f9bbcdae488fba61c8c107542802
Jess Edmund Fan <jessefan2002@gmail.com>
2021-02-04 10:53:44 -0800

    update

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import discord
 from interact_web import ResponseGenerator
 import json
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 
 intents = discord.Intents(messages=True, guilds=True, typing = False, presences = False, members=False)
 client = discord.AutoShardedClient(intents=intents, chunk_guilds_at_startup=False)
@@ -18,18 +19,26 @@ async def on_ready():
     print('--------')
     print(str(len(client.shards))+" shard(s)")
     
+enabled_guilds=[387328200113651732,669808453497258024]
+    
 @client.event
 async def on_message(message):
     loop = asyncio.get_event_loop()
     if message.author.bot == False and message.guild != None:
         if message.content.lower().startswith(config_general["prefix"]):
-            if message.guild.id == 387328200113651732:
+            if message.guild.id in enabled_guilds:
                 message.content=message.content[len(config_general["prefix"]):]
                 if message.content == "-h":
-                    await message.reply("History:\n> "+"\n> ".join(model.history))
+                    logs=model.history[message.author.id]
+                    await message.reply("History:\n> "+"\n> ".join(logs["history"])+f"\nLast seen: {datetime.fromtimestamp(logs['timestamp'])}")
+                if message.content == "-r":
+                    model.reset(message.author.id)
+                    await message.reply("Successfully reset your history with Jade")
                 else:
-                    await message.channel.trigger_typing()
-                    out = await loop.run_in_executor(ThreadPoolExecutor(), model.response, message.author.display_name, message.content, True)
-                    await message.reply(out.replace("@everyone","").replace("@here", ""))
+                    async with message.channel.typing():
+                        out = await loop.run_in_executor(ThreadPoolExecutor(), model.response, message.author, message.content, True)
+                        await message.reply(out.replace("@everyone","").replace("@here", ""))
+            else:
+                await message.reply("This sever does not have the test model enabled. This process must be done manually by Jess- Join the support server here: https://discord.com/invite/eGffUFY")
 
 client.run(config_general["token"])
---
+++ src/response.py
@@ -0,0 +1,54 @@
+import re
+import requests
+import json
+import time
+
+class ResponseGenerator:    
+    def __init__(self, url): 
+        self.history={}
+        self.url=url
+        self.r1=re.compile(r'https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)|[\w\-\.]+@(?:[\w-]+\.)+[\w-]{2,4}|(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}|```(?:.?)+```|:[^:\s]*(?:::[^:\s]*)*:|(?:\\n)+|(?<=[:.,!?()]) (?=[:.,!?()])|[^a-z1-9.,!@?\"\'\s\/\U0001F600-\U0001F64F\U0001F300-\U0001F5FF]+', flags=re.DOTALL | re.IGNORECASE)
+        self.r2=re.compile(r'[\U00003000\U0000205F\U0000202F\U0000200A\U00002000-\U00002009\U00001680\U000000A0\t]+')
+        self.r5=re.compile(r"([\.\'\"@?!a-z])\1{4,}", re.IGNORECASE)
+        self.r6=re.compile(r"\s(.+?)\1+\s", re.IGNORECASE)
+        self.r8=re.compile(r"([\s!?@\"\'])\1+")
+        self.r9=re.compile(r'\s([?.!\"](?:\s|$))')
+        
+    def register(self, id, timestamp):
+        if id in self.history:
+            if time.time()-self.history[id]["timestamp"] >= 600:
+                self.history[id]={"history":[],"timestamp":timestamp}
+            else:
+                self.history[id]["timestamp"]=timestamp
+        else:
+            self.history[id]={"history":[],"timestamp":timestamp}
+    
+    def reset(self, id):
+        self.history[id]={"history":[],"timestamp":time.time()}
+        
+    def clean(self, text, author=False):
+        text= re.sub(self.r1, "", text.strip()) #remove urls, emails, code blocks, custom emojis, spaces between punctuation, non-emoji, punctuation, letters, and phone numbers
+        text= re.sub(self.r2, " ", text) #handle... interesting spaces
+        text= re.sub(self.r5, r"\1\1\1", text) #handle excessive repeats of punctuation, limited to 3
+        text= re.sub(self.r6, r" \1 ", text) #handle repeated words
+        text= re.sub(self.r8, r"\1",text) #handle excessive spaces or excessive punctuation
+        text= re.sub(self.r9, r'\1', text) #handle spaces before punctuation but after text
+        text= text.strip().replace("\n","/n") #handle newlines
+        text=text.strip()
+        text=text.replace('"',"'")
+        if author==True: text=text.split(" ")[-1]
+        return text
+
+    def response(self, user, inp, debug=False):
+        self.register(user.id, time.time())
+        inp= self.clean(user.display_name, author=True)+": "+self.clean(inp)
+        inpdata = '{"inputs": ["Input: '+'/b'.join(self.history+[inp])+'"]}'
+        response = requests.post(self.url.encode("utf-8"), data=inpdata.encode("utf-8"))
+        message = json.loads(response.text)
+        if "error" in message or debug: 
+            print(f"{message}")
+            if "error" in message: return str(message)
+        self.history[user.id]["history"].append(inp)
+        self.history[user.id]["history"].append("Jade: "+message["outputs"]["outputs"][0])
+        self.history[user.id]["history"] = self.history[user.id]["history"][-10:]
+        return message["outputs"]["outputs"][0].replace("/n", "\n")
