15bc8a1f415731dfc9ef547ba528b2609e2794d1
Jess Edmund Fan <jessefan2002@gmail.com>
2021-03-22 14:47:42 -0700

    why pass to a config file then to another file when you don't need to?

--- src/createtask.py
+++ src/createtask.py
@@ -1,31 +1,21 @@
 import t5
-import json
 import functools
 import numpy as np
 import tensorflow.compat.v1 as tf
 import tensorflow_datasets as tfds
 
-with open("config.json", "r") as f:
-    nq_tsv_path=json.load(f)
-
 def nq_dataset_fn(split, shuffle_files=False):
+    global nq_tsv_path
     # We only have one file for each split.
     del shuffle_files
     # Load lines from the text file as examples.
-    ds = tf.data.TextLineDataset(nq_tsv_path[split])
-    #ds.shard(8, 8)
-    #ds.shuffle(buffer_size=10000)
+    ds = tf.data.TextLineDataset(nq_tsv_path[split], compression_type="GZIP")
     # Split each "<question>\t<answer>" example into (question, answer) tuple.
-    ds = ds.map(
-    functools.partial(tf.io.decode_csv, field_delim="\t", use_quote_delim=False),
-                      num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    
+    ds = ds.shuffle(buffer_size=10000)
+    ds = ds.map(functools.partial(tf.io.decode_csv, record_defaults=["",""], field_delim="\t", use_quote_delim=False),
+                num_parallel_calls=tf.data.experimental.AUTOTUNE)
     ds = ds.map(lambda *ex: dict(zip(["question", "answer"], ex)))
     return ds
-
-print("A few raw validation examples...")
-for ex in tfds.as_numpy(nq_dataset_fn("validation").take(5)):
-    print(ex)
 
 def preprocess(ds):
     def normalize_text(text):
@@ -36,22 +26,29 @@ def preprocess(ds):
     def to_inputs_and_targets(ex):
         """Map {"question": ..., "answer": ...}->{"inputs": ..., "targets": ...}."""
         return {
-            "inputs": tf.strings.join(["Input: ", normalize_text(ex["question"])]),
+            "inputs": normalize_text(ex["question"]),
             "targets": normalize_text(ex["answer"])
         }
     return ds.map(to_inputs_and_targets, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-  
-t5.data.TaskRegistry.add(
-    nq_tsv_path["taskname"],
-    # Specify the task type.
-    t5.data.Task,
-    # Supply a function which returns a tf.data.Dataset.
-    dataset_fn=nq_dataset_fn,
-    splits=["train", "validation"],
-    # Supply a function which preprocesses text from the tf.data.Dataset.
-    text_preprocessor=[preprocess],
-    # Lowercase targets before computing metrics.
-    postprocess_fn=t5.data.postprocessors.lower_text, 
-    # We'll use accuracy as our evaluation metric.
-    metric_fns=[t5.evaluation.metrics.accuracy]
-)
+
+
+def create_registry(train, val, taskname):
+    global nq_tsv_path
+    nq_tsv_path={"train":train, "validation":val}
+    print("A few raw validation examples...")
+    for ex in tfds.as_numpy(nq_dataset_fn("validation").take(5)):
+        print(ex)
+    t5.data.TaskRegistry.add(
+        taskname,
+        # Specify the task type.
+        t5.data.Task,
+        # Supply a function which returns a tf.data.Dataset.
+        dataset_fn=nq_dataset_fn,
+        splits=["train", "validation"],
+        # Supply a function which preprocesses text from the tf.data.Dataset.
+        text_preprocessor=[preprocess],
+        # Lowercase targets before computing metrics.
+        postprocess_fn=t5.data.postprocessors.lower_text, 
+        # We'll use accuracy as our evaluation metric.
+        metric_fns=[t5.evaluation.metrics.accuracy]
+    )
