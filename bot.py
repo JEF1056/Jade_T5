7ac4ec110ce0a0d146c14af706165c8492aa7f9f
Jess Edmund Fan <jessefan2002@gmail.com>
2021-03-26 17:10:45 -0700

    add fixes

--- bot.py
+++ bot.py
@@ -20,26 +20,21 @@ async def on_ready():
     print('--------')
     print(str(len(client.shards))+" shard(s)")
     
-enabled_guilds=[387328200113651732,669808453497258024, 692688645559418930]
-    
 @client.event
 async def on_message(message):
     loop = asyncio.get_event_loop()
     if message.author.bot == False and message.guild != None:
         if message.content.lower().startswith(config_general["prefix"]):
-            if message.guild.id in enabled_guilds:
-                message.content=message.content[len(config_general["prefix"]):]
-                if message.content == "-h":
-                    logs=model.register(message.author.id, time.time())
-                    await message.reply("History:\n> "+"\n> ".join(logs["history"])+f"\nLast seen: {datetime.fromtimestamp(logs['timestamp'])}")
-                elif message.content == "-r":
-                    model.reset(message.author.id)
-                    await message.reply("Successfully reset your history with Jade")
-                else:
-                    async with message.channel.typing():
-                        out = await loop.run_in_executor(ThreadPoolExecutor(), model.response, message.author, message.content, True)
-                        await message.reply(out.replace("@everyone","").replace("@here", ""))
+            message.content=message.content[len(config_general["prefix"]):]
+            if message.content == "-h":
+                logs=model.register(message.author.id, time.time())
+                await message.reply("History:\n> "+"\n> ".join(logs["history"])+f"\nLast seen: {datetime.fromtimestamp(logs['timestamp'])}")
+            elif message.content == "-r":
+                model.reset(message.author.id)
+                await message.reply("Successfully reset your history with Jade")
             else:
-                await message.reply("This sever does not have the test model enabled. This process must be done manually by Jess- Join the support server here: https://discord.com/invite/eGffUFY")
+                async with message.channel.typing():
+                    out = await loop.run_in_executor(ThreadPoolExecutor(), model.response, message.author, message.content, True)
+                await message.reply(out.replace("@everyone","").replace("@here", ""))
 
 client.run(config_general["token"])
